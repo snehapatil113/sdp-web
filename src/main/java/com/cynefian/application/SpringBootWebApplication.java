ecdcfab0c31a078bf8d72e4b86d07fb36bc70397
sreenidhi <sreenidhi@localhost>
2022-04-27 23:18:24 +0530

    pilot

---
+++ src/main/java/com/cynefian/application/SpringBootWebApplication.java
@@ -0,0 +1,26 @@
+package com.cynefian.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(SpringBootWebApplication.class);
+	  }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
